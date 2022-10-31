b8c55a305b450b029078f0a82d1b8b3ecf3a0a52
cmcgartoll <56743782+cmcgartoll@users.noreply.github.com>
2022-10-30 17:10:20 -0700

    feat(home page): setup

---
+++ backend/frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <img
+          src="https://preview.redd.it/w2naip2vur421.jpg?auto=webp&s=3438c744ab37c316f1f71250536655aa82ddf812"
+          width="300"
+          className="img-thumbnail"
+          style={{ marginTop: "20px" }}
+        />
+        <hr />
+        <h5>
+          <i>Yo</i>
+        </h5>
+        <h1>This is Kanye Album Ratings</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
