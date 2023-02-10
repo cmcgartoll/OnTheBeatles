43dcfdc54c061cae603cd48277d591d5cb40623b
cmcgartoll <56743782+cmcgartoll@users.noreply.github.com>
2023-02-10 11:55:12 -0800

    feat (backend): allowed hosts to *

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ty$@zkuszrwf%wuam)*t306_b*!@w3xx%l747y-9%4($v2bo_6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
